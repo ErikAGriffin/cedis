78ecc76015425fd1c3ceea77e2b6b249b1f6e846
Erik Griffin <griffin.erik@gmail.com>
2020-05-30 11:58:40 -0600

    First tests in place
    Testing the get, set, and del methods

--- spec/cedis_spec.cr
+++ spec/cedis_spec.cr
@@ -1,9 +1,35 @@
 require "./spec_helper"
 
-describe Cedis do
-  # TODO: Write tests
+private def init_tests
+  Cedis::Store.new
+end
 
-  it "works" do
-    false.should eq(true)
+describe Cedis do
+
+  describe Cedis::Store do
+
+    describe "#get, #set, and #del" do
+
+      it "can #set a value, then #get it" do
+        store = init_tests
+        store.set "foo", "bar"
+        store.get("foo").should eq "bar"
+      end
+
+      it "#get returns nil if there is no value present" do
+        store = init_tests
+        store.get("foo").should be_nil
+      end
+
+      it "#del removes a key" do
+        store = init_tests
+        store.set "foo", "bar"
+        store.del "foo"
+        store.get("foo").should eq nil
+      end
+
+    end
+
   end
+
 end
--- src/cedis.cr
+++ src/cedis.cr
@@ -1,6 +1,33 @@
-# TODO: Write documentation for `Cedis`
+# Cedis is an in-memory data store that
+# mirrors the API of Redis.
+# ======
+# This was initially given to me as a coding challenge
+# at a job interview. The requirements:
+#
+# Must be able to set, get, and del keys.
+# Must be able to support transactions.
+# Transactions start with a #begin statement,
+#   can be canceled with #abort,
+#   and the operations in a transaction only complete
+#   if every operation succeed.
+# Must also support nested transactions.
 module Cedis
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Store
+    @store = Hash(String,String | Nil).new(nil)
+
+    def get(key)
+      @store[key]
+    end
+
+    def set(key, value)
+      @store[key] = value
+    end
+
+    def del(key)
+      @store.delete key
+    end
+  end
+
 end
