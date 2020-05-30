a9c9c0ab13527252a8c5fc45d25975f387763ac1
Erik Griffin <griffin.erik@gmail.com>
2020-05-30 12:18:13 -0600

    Corrected requirements: Getting a missing key raises a KeyError

--- spec/cedis_spec.cr
+++ spec/cedis_spec.cr
@@ -5,31 +5,28 @@ private def init_tests
 end
 
 describe Cedis do
-
   describe Cedis::Store do
-
     describe "#get, #set, and #del" do
-
       it "can #set a value, then #get it" do
         store = init_tests
         store.set "foo", "bar"
         store.get("foo").should eq "bar"
       end
 
-      it "#get returns nil if there is no value present" do
+      it "#get raises an error if there is no value present" do
         store = init_tests
-        store.get("foo").should be_nil
+        expect_raises(KeyError) { store.get "foo" }
       end
 
       it "#del removes a key" do
         store = init_tests
         store.set "foo", "bar"
         store.del "foo"
-        store.get("foo").should eq nil
+        expect_raises(KeyError) { store.get "foo" }
       end
-
     end
 
+    describe "#transaction and #abort" do
+    end
   end
-
 end
--- src/cedis.cr
+++ src/cedis.cr
@@ -1,21 +1,24 @@
 # Cedis is an in-memory data store that
 # mirrors the API of Redis.
+#
 # ======
+#
 # This was initially given to me as a coding challenge
 # at a job interview. The requirements:
 #
-# Must be able to set, get, and del keys.
-# Must be able to support transactions.
-# Transactions start with a #begin statement,
+# - Must be able to set, get, and del keys.
+# - Attempting to get a key which does not exist raises an error
+# - Must be able to support transactions.
+# - Transactions start with a #begin statement,
 #   can be canceled with #abort,
 #   and the operations in a transaction only complete
 #   if every operation succeed.
-# Must also support nested transactions.
+# - Must also support nested transactions.
 module Cedis
   VERSION = "0.1.0"
 
   class Store
-    @store = Hash(String,String | Nil).new(nil)
+    @store = Hash(String, String).new
 
     def get(key)
       @store[key]
@@ -29,5 +32,4 @@ module Cedis
       @store.delete key
     end
   end
-
 end
